00c151fe5448ab0d2c6ee3de3b626ee251a70a65
Teona Gogichashvili <teonafreckling@gmail.com>
2023-02-14 23:08:00 +0300

    added task2

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿int Max(int arg1, int arg2, int arg3)
+
+{
+    int result = arg1;
+    if (arg2>result) result = arg2;
+    if (arg3>result) result = arg3;
+    return result;
+}
+
+int[] array = {12, 7, 78, 90, 55, 32, 12, 98};
+array [0] = 6;
+
+
+Console.WriteLine(array[0]);
