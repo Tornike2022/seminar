525e60f97325c903cde75132afca1a82475e165b
Teona Gogichashvili <teonafreckling@gmail.com>
2023-02-15 11:42:29 +0300

    added task3

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {1, 6, 3, 13, 86, 4, 75, 44, 3, 90};
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index ++;
+    //index= index+1
+}
