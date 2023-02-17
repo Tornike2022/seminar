6d5920b5e5eafe8402a9b413cb2d29409e928354
Teona Gogichashvili <teonafreckling@gmail.com>
2023-02-17 16:07:02 +0300

    added task6

---
+++ task6/Program.cs
@@ -0,0 +1,10 @@
+﻿//выводит случайное число из отрезка [10, 99] 
+//и показывает наибольшую цифру числа
+Console.Clear();
+int num = new Random().Next(10, 100);
+int a1 = num/10;
+int a2 = num%10;
+int max = a1;
+if (max<a1)
+    max=a1;
+Console.WriteLine($"maximum digit of {num} -> {max}");
