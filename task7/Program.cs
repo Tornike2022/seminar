af216f0ef4feb2f243eae0679537d6bf836542ae
Teona Gogichashvili <teonafreckling@gmail.com>
2023-02-17 17:15:10 +0300

    added task7

---
+++ task7/Program.cs
@@ -0,0 +1,8 @@
+﻿//напишите программу, которая выводит случайное 
+//трехзначное число и удаляет вторую цифру этого числа
+Console.Clear();
+int num = new Random().Next(100, 1000);
+int a1 = num/100;
+int a2 = num%10;
+
+Console.WriteLine($"del mid digit of {num} -> {a1}{a2}");
