3951eb1564d95403c6672da43ed9889a8617de08
Teona Gogichashvili <teonafreckling@gmail.com>
2023-02-18 13:01:13 +0300

    added task9

---
+++ task9/Program.cs
@@ -0,0 +1,14 @@
+﻿//принимает на вход число и проверяет кратно ли оно одновременно 7 и 23
+Console.Write("a: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a % (7*23) == 0)
+{
+    Console.WriteLine("кратно");
+
+}
+
+else
+{
+    Console.WriteLine("не кратно");
+}
