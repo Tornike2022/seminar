a007fed5a37059c6927356a54591c6166cd31a62
Teona Gogichashvili <teonafreckling@gmail.com>
2023-02-18 19:19:33 +0300

    added task11

---
+++ task11/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа , 
+//или сообщает, что третьей цифры нет
+Console.Write("a: ");
+string a = Console.ReadLine();
+if (a.Length==3)
+{
+    Console.WriteLine(a.Substring(2));
+
+}
+
+else
+{
+    Console.WriteLine("нет третьей цифры");
+}
