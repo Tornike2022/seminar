d3abce2934c952666eb35660829d60458d21bcbe
Teona Gogichashvili <teonafreckling@gmail.com>
2023-02-18 18:19:31 +0300

    added task10

---
+++ task10/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+
+Console.Write("a: ");
+int a = int.Parse(Console.ReadLine());
+int a1 = a/10; 
+int a2 = a1%10; 
+
+Console.WriteLine($"second digit of {a} -> {a2}");
