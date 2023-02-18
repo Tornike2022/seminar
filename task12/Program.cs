abf970954953d92c500ded79ed2600fffd24d5a3
Teona Gogichashvili <teonafreckling@gmail.com>
2023-02-18 19:40:56 +0300

    added task12

---
+++ task12/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день
+// недели, и проверяет, является ли этот день выходным. 6 -> да
+Console.Clear();
+
+Console.WriteLine("введите цифру, обозначающую день недели: ");
+int a = int.Parse(Console.ReadLine());
+if (a == 1) {Console.WriteLine($"{a} -> нет");}
+else if (a == 2) {Console.WriteLine($"{a} -> нет");}
+else if (a == 3) {Console.WriteLine($"{a} -> нет");}
+else if (a == 4) {Console.WriteLine($"{a} -> нет");}
+else if (a == 5) {Console.WriteLine($"{a} -> нет");}
+else if (a == 6) {Console.WriteLine($"{a} -> да");}
+else if (a == 7) {Console.WriteLine($"{a} -> да");}
+
