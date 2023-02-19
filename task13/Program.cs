8ec138e0cb8186122a9c255db90de1aac7876282
Teona Gogichashvili <teonafreckling@gmail.com>
2023-02-19 18:35:20 +0300

    added task13

---
+++ task13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите Х1: ");
+double x1 = double.Parse(Console.ReadLine());
+Console.Write("Введите Y1: ");
+double y1 = double.Parse(Console.ReadLine());
+Console.Write("Введите X2: ");
+double x2 = double.Parse(Console.ReadLine());
+Console.Write("Введите Y2: ");
+double y2 = double.Parse(Console.ReadLine());
+double d1 = Math.Sqrt(-5);
+double d = Math.Sqrt(Math.Pow(x1-x2, 2)+ Math.Pow(y1-y2, 2));
+Console.WriteLine($"d={d:f3}, {d1}");
