f7948fbb1b40da0d27abb461d99047300080f290
Teona Gogichashvili <teonafreckling@gmail.com>
2023-02-21 17:30:39 +0300

    added task14

---
+++ task14/Program.cs
@@ -0,0 +1,10 @@
+﻿// принимает на вход число N, и выдает таблицу квадратов чисел от 1 до N
+// 5 -> 1, 4, 9, 16, 25
+Console.Clear();
+Console.Write("введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int[] mas = new int [N]; // int [mas] - массив из целых чисел
+for (int i = 1; i<=N; i++)
+    mas[i - 1] = i * i;
+for (int i = 0; i<N; i++)
+    Console.WriteLine($"{mas[i]}");
