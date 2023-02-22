3cf10ddfff25c75873267e618071d9b6eade0a31
Teona Gogichashvili <teonafreckling@gmail.com>
2023-02-22 15:40:16 +0300

    added task16

---
+++ task16/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и 
+// проверяет, является ли оно палиндромом.
+Console.WriteLine("введите пятизначное число");
+string n = Console.ReadLine();
+string r = string.Concat(n.Reverse());
+if (n==r)
+{
+    Console.WriteLine("палиндром");
+}
+
+else
+{
+    Console.WriteLine("не палиндром");
+}
+
+    
+    
