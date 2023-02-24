f9a6f7ea29286ca530613cc7f5638e4ffc716618
Teona Gogichashvili <teonafreckling@gmail.com>
2023-02-24 18:16:19 +0300

    цикл for

---
+++ cycleFOR/Program.cs
@@ -0,0 +1,13 @@
+﻿// //строчку с компонуем друг за дружкой count раз
+string Method4(int count, string text) 
+{
+    string result = String.Empty;
+    for(int i = 0; i<count; i++)
+    {
+        result = result + text;
+     
+    }
+    return result;
+}
+string res = Method4(10, "asdf");
+Console.WriteLine(res); //получаем asdf 10 раз
