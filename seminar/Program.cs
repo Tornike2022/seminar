418316923ad2c7c18cdb7f24249b9f8dff4c665d
Teona Gogichashvili <teonafreckling@gmail.com>
2023-02-25 19:14:32 +0300

    сумма числел от 1 до n

---
+++ seminar/Program.cs
@@ -0,0 +1,12 @@
+﻿// принимает на вход число а и выдает сумму чисел от 1 до а
+int GetSum(int N){
+    int sum = 0;
+    for (int i = 1; i <= N; i++)
+        sum+=i;
+    return sum;
+}
+Console.Clear();
+Console.Write("введите число N: ");
+int N = int.Parse(Console.ReadLine());
+// GetSum(n);
+Console.WriteLine($"Сумма от 1 до {N} равна {GetSum(N)}");
