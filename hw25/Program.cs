d1a16fda92fa4d0d6f67bc3e65e481ca6ecfd707
Teona Gogichashvili <teonafreckling@gmail.com>
2023-02-25 22:03:32 +0300

    возведение в степень

---
+++ hw25/Program.cs
@@ -0,0 +1,7 @@
+﻿//напишите цикл, кот принимает на вход 2 числа 
+//а и в и возводит число а в натур степень в (3,5 -> 243)
+Console.Write("a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("b: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine($"{Math.Pow(a, b)}");
