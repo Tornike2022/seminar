cc41152e3dda12ef813f62b9e64706d1417312e4
Teona Gogichashvili <teonafreckling@gmail.com>
2023-02-25 19:40:10 +0300

    кол-во цифр в числе

---
+++ seminar2/Program.cs
@@ -0,0 +1,19 @@
+﻿// программу кот принимает на вход число и выдает кол-во цифр в числе
+
+Console.Clear();
+Console.Write("введите число: ");
+int s = int.Parse(Console.ReadLine());
+Console.WriteLine($"количество цифр = {CountNaumbers(s)}");
+int CountNaumbers(int n) //123
+{
+    int count = 0; // count 0
+    if (n<0)
+        n*= (-1); //n=n*(-1)
+    while (n>0) // 0>0
+    {
+        n/=10; //n=n/10   n=0
+        count ++; //count 3
+    }
+    return count;
+
+}
