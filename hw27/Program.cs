5ead35bda4a3a65b6cb561f38ec456e933685427
Teona Gogichashvili <teonafreckling@gmail.com>
2023-02-25 21:46:08 +0300

    сумма цифр в числе

---
+++ hw27/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите прошграмму кот принимает на вход число и выдает 
+// сумму цифр в числе (452->11)
+int a = int.Parse(Console.ReadLine());
+            int s = 0;
+            while (a > 0)
+            {
+ 
+                s = s + a % 10;
+                a = a /10 ;
+ 
+            }
+ Console.WriteLine(s);
